a9bd702fb779caefc7ea145340b7b2ca7ba338b4
valenbellamy <bellamyvalentin@gmail.com>
2025-05-28 10:09:56 +0200

    fix: try to fix again

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -20,7 +20,15 @@ export const Seo = ({ title, description, pathname, children }) => {
     <>
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
+      <meta
+        name="image"
+        content="https://clementkarl2924.netlify.app/static/9e7312766bc08c23c60410ff2f3bb362/19eb8/Jeu_3.webp"
+      />
+      <meta
+        property="og:image"
+        content="https://clementkarl2924.netlify.app/static/9e7312766bc08c23c60410ff2f3bb362/19eb8/Jeu_3.webp"
+        data-gatsby-head="true"
+      ></meta>
       <link
         rel="icon"
         href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='80'>🎨</text></svg>"
